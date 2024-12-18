5451bbe3aae542c572f1009160240b00a0cf80c8
Tôn Thất Hữu Luân <s3958304@rmit.edu.vn>
2024-12-18 18:46:31 +0700

    Update webpack.dev.js
    Change Host in the webpack file to start the server locally

--- client/webpack/webpack.dev.js
+++ client/webpack/webpack.dev.js
@@ -78,7 +78,7 @@ const config = {
   devServer: {
     // Configuration for webpack-dev-server
     port: 8080,
-    host: '0.0.0.0', // Listen on all network interfaces
+    host: 'localhost', // Listen on all network interfaces
     open: true,
     inline: true,
     compress: true,
